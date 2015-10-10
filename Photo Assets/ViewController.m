0d633562e4f24dd0cfe0af78fcedc5db3c99f826
Jay Versluis <jay@versluis.com>
2015-10-10 12:03:41 -0400

    testing things

--- Photo Assets/ViewController.m
+++ Photo Assets/ViewController.m
@@ -12,6 +12,7 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) NSMutableArray *images;
+@property (nonatomic, strong) NSMutableArray *resources;
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
@@ -22,6 +23,8 @@
     [super viewDidLoad];
     
     // NSLog(@"Frigging Degraded key is %@", (NSString *)PHImageResultIsDegradedKey);
+    
+    [self readAssets];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,6 +38,14 @@
         _images = [[NSMutableArray alloc]init];
     }
     return _images;
+}
+
+- (NSMutableArray *)assets {
+    
+    if (!_resources) {
+        _resources = [[NSMutableArray alloc]init];
+    }
+    return _resources;
 }
 
 # pragma mark - Photo Library Methods
@@ -53,7 +64,17 @@
             // add image to our array - if it's hi-res
             if ([[info valueForKey:PHImageResultIsDegradedKey] isEqualToNumber:@0]) {
                 [self.images addObject:result];
-                NSLog(@"Asset added: %@", [result description]);
+                NSLog(@"Image added: %@", [result description]);
+                
+                // and also add the PHAssetResource for "full picture"
+                NSArray *resources = [PHAssetResource assetResourcesForAsset:asset];
+                
+                for (PHAssetResource *resource in resources) {
+                    if (resource.type == PHAssetResourceTypeFullSizePhoto) {
+                        [self.resources addObject:asset];
+                        NSLog(@"Asset added: %@", [asset description]);
+                    }
+                }
             }
         }];
     }
@@ -66,6 +87,50 @@
     self.imageView.image = [self.images objectAtIndex:(NSInteger)i];
 }
 
+- (IBAction)saveImageData:(id)sender {
+    
+    // save the first asset as data
+    PHAssetResource *resource = self.resources.firstObject;
+    PHAssetResourceManager *manager = [PHAssetResourceManager defaultManager];
+    
+    [manager requestDataForAssetResource:resource options:nil dataReceivedHandler:^(NSData * _Nonnull data) {
+        // receive data here
+        NSURL *dataURL = [[self applicationDocumentsDirectory]URLByAppendingPathComponent:@"AssetData"];
+        
+        if (![data writeToURL:dataURL atomically:YES]) {
+            NSLog(@"Data File has been saved successfully");
+        } else {
+            NSLog(@"There was a problem saving the data file.");
+        }
+        
+        // or
+        
+        [manager writeDataForAssetResource:resource toFile:dataURL options:nil completionHandler:^(NSError * _Nullable error) {
+            NSLog(@"Data written - result was %@", error.localizedDescription);
+        }];
+        
+        
+    } completionHandler:^(NSError * _Nullable error) {
+        // called when this thing is finished
+        NSLog(@"Asset manager has finished with Error Code: %@", error.localizedDescription);
+    }];
+}
+
+- (IBAction)loadImageData:(id)sender {
+    
+    // load data file
+    NSURL *dataURL = [[self applicationDocumentsDirectory]URLByAppendingPathComponent:@"AssetData"];
+    NSData *assetData = [[NSFileManager defaultManager]contentsAtPath:dataURL.path];
+    PHAssetResource *resource = [[PHAssetResource alloc]init];
+    
+    
+}
+
+// Returns the URL to the application's Documents directory.
+- (NSURL *)applicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
 
 
 @end
